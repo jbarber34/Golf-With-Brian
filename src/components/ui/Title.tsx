6bda8a347b6704a62dd3ddfb7f122b61685ed72b
James Barber <jamesbarber@twinsbaseball.com>
2022-11-22 15:16:49 -0500

    Fix title padding

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -4,12 +4,34 @@ import { faGolfBall } from '@fortawesome/free-solid-svg-icons';
 
 export default function Title() {
   return (
-    <div className='bg-brianBlue text-white grid grid-cols-2'>
-      <div className='m-auto text-3xl w-2/3 font-extrabold'>
+    <div className='bg-brianBlue text-white grid grid-cols-1 lg:grid-cols-2'>
+      <div className='mt-16 m-auto text-3xl w-2/3 font-extrabold'>
         <h1>
           Learn to golf from the best instructor at his apartment complex.
         </h1>
-        <ul className='items-center flex mt-4 space-x-6 space-y-0'>
+        <div className='grid grid-cols-1 xl:grid-cols-2 justify-between py-8'>
+          <div className='xl:text-2xl w-64'>
+            <Link href='https://www.cobragolf.com/'>
+              <button className='bg-slate-900 border-solid border-2 border-slate-900 hover:bg-twinsGold text-white font-bold py-2 px-4 rounded-md'>
+                <a>
+                  <FontAwesomeIcon icon={faGolfBall} className='fa-md' />{' '}
+                  Cobra/Puma
+                </a>
+              </button>
+            </Link>
+          </div>
+          <div className='xl:text-2xl w-64 pt-4 xl:pt-0'>
+            <Link href='https://www.aero-swing.com/'>
+              <button className='bg-clear border-solid border-2 hover:bg-twinsGold text-white font-bold py-2 px-4 rounded-md'>
+                <a>
+                  <FontAwesomeIcon icon={faGolfBall} className='fa-md' />{' '}
+                  Aero-Swing
+                </a>
+              </button>
+            </Link>
+          </div>
+        </div>
+        {/* <ul className='items-center flex mt-4 space-x-6 space-y-0 min-w-full'>
           <li className='text-black'>
             <Link href='https://www.cobragolf.com/'>
               <button className='bg-slate-900 hover:bg-twinsGold text-white font-bold py-2 px-4 rounded-md'>
@@ -30,7 +52,7 @@ export default function Title() {
               </button>
             </Link>
           </li>
-        </ul>
+        </ul> */}
       </div>
       <div>
         <img
