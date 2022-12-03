ad57b22cbe38131c3cd6ab33a5029c275a966444
James Barber <jamesbarber@twinsbaseball.com>
2022-12-03 14:58:50 -0500

    Add sponsors section to index.tsa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import Features from '~/components/ui/Features';
-import Services from '~/components/ui/Services';
+// import Services from '~/components/ui/Services';
 import Testimonials from '~/components/ui/Testimonials';
 import Title from '~/components/ui/Title';
 import Lessons from '~/components/ui/Lessons';
+import Sponsors from '~/components/ui/Sponsors';
 
 function Home() {
   return (
     <div>
       <Title />
       <Features />
-      <Services />
+      {/* <Services /> */}
       <Testimonials />
       <Lessons />
+      <Sponsors />
     </div>
   );
 }
