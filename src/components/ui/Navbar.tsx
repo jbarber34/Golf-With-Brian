0ca627f2c0d7e0aa385374d6f5d29f4b7b0a7e6c
James Barber <jpbarber34@gmail.com>
2023-10-12 13:59:12 -0400

    Initial push of GWB redesign

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,157 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { useRouter } from 'next/router';
+
+export function Navbar() {
+  const router = useRouter();
+
+  return (
+    <Disclosure
+      as='nav'
+      className='fixed bg-brianBlue top-0 left-0 right-0 shadow z-50'
+    >
+      {({ open }) => (
+        <>
+          <div className='mx-auto max-w-7xl px-2 sm:px-4 lg:px-8'>
+            <div className='flex h-16 justify-between'>
+              <div className='flex px-2 lg:px-0'>
+                <div className='flex flex-shrink-0 items-center'>
+                  <Link passHref href='/'>
+                    <Image
+                      className='block h-8 w-auto lg:hidden hover:animate-bump'
+                      src='/Golf_With_Brian_Text.png'
+                      alt='Golf with Brian'
+                      height='40'
+                      width='166'
+                    />
+                  </Link>
+                </div>
+              </div>
+              <div className='hidden lg:ml-6 lg:flex lg:space-x-8'>
+                <Link href='/'>
+                  <a
+                    className={
+                      router.pathname == '/'
+                        ? 'inline-flex items-center border-b-2 border-legacyGold px-1 pt-1 text-sm font-medium text-white'
+                        : 'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-legacyGold hover:text-legacyGold'
+                    }
+                  >
+                    Home
+                  </a>
+                </Link>
+                <Link href='/About'>
+                  <a
+                    className={
+                      router.pathname == '/About'
+                        ? 'inline-flex items-center border-b-2 border-legacyGold px-1 pt-1 text-sm font-medium text-white'
+                        : 'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-legacyGold hover:text-legacyGold'
+                    }
+                  >
+                    About Brian
+                  </a>
+                </Link>
+                <Link href='/#lessons'>
+                  <a
+                    className={
+                      router.pathname == '/#lessons'
+                        ? 'inline-flex items-center border-b-2 border-legacyGold px-1 pt-1 text-sm font-medium text-white'
+                        : 'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-legacyGold hover:text-legacyGold'
+                    }
+                  >
+                    Lessons
+                  </a>
+                </Link>
+                <Link href='/#contact'>
+                  <a
+                    className={
+                      router.pathname == '/#contact'
+                        ? 'inline-flex items-center border-b-2 border-legacyGold px-1 pt-1 text-sm font-medium text-white'
+                        : 'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-legacyGold hover:text-legacyGold'
+                    }
+                  >
+                    Contact Brian
+                  </a>
+                </Link>
+                <Link href='/#sponsors'>
+                  <a
+                    className={
+                      router.pathname == '/#sponsors'
+                        ? 'inline-flex items-center border-b-2 border-legacyGold px-1 pt-1 text-sm font-medium text-white'
+                        : 'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-legacyGold hover:text-legacyGold'
+                    }
+                  >
+                    Sponsors
+                  </a>
+                </Link>
+              </div>
+              <div className='flex items-center lg:hidden'>
+                {/* Mobile menu button */}
+                <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-legacyGold'>
+                  <span className='sr-only'>Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
+                  ) : (
+                    <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className='lg:hidden'>
+            <div className='space-y-1 pt-2 pb-3'>
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
+              <Link href='/'>
+                <a
+                  className={
+                    'block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-twinsRed hover:bg-gray-50 hover:text-gray-800'
+                  }
+                >
+                  Home
+                </a>
+              </Link>
+              <Link href='/About'>
+                <a
+                  className={
+                    'block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-twinsRed hover:bg-gray-50 hover:text-gray-800'
+                  }
+                >
+                  About Brian
+                </a>
+              </Link>
+              <Link href='/#lessons'>
+                <a
+                  className={
+                    'block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-twinsRed hover:bg-gray-50 hover:text-gray-800'
+                  }
+                >
+                  Lessons
+                </a>
+              </Link>
+              <Link href='/#contact'>
+                <a
+                  className={
+                    'block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-twinsRed hover:bg-gray-50 hover:text-gray-800'
+                  }
+                >
+                  Contact Brian
+                </a>
+              </Link>
+              <Link href='/#sponsors'>
+                <a
+                  className={
+                    'block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-twinsRed hover:bg-gray-50 hover:text-gray-800'
+                  }
+                >
+                  Sponsors
+                </a>
+              </Link>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
---
+++ src/pages/About.tsx
@@ -0,0 +1,111 @@
+export default function About() {
+  return (
+    <div>
+      <section id='aboutBrian'>
+        <div className='lg:px-36 pt-12 pb-8'>
+          <div className='px-36 bg-white'>
+            <h1
+              className='container-fluid text-center text-3xl mt-8
+        before:inline-block before:lg:w-52 before:md:w-40 before:sm:w-24 before:w-24 before:h-1 before:bg-legacyGold before:mb-2 before:mr-8
+        after:inline-block after:lg:w-52 after:md:w-40 after:sm:w-24 after:w-24 after:h-1 after:bg-legacyGold after:mb-2 after:ml-8'
+            >
+              About Brian
+            </h1>
+            <p className='py-4 bg-white indent-8'>
+              Brian grew up in a small town in Upstate NY called Margaretville.
+              He was a star baseball player and an honor roll student who spent
+              his summers and weekends working at the local golf course, Hanah
+              Mountain Resort and Country Club, working as a cart boy and a
+              dishwasher at night.
+            </p>
+            <p className='pb-4 indent-8'>
+              Throughout his high school years he worked at Hanah and learned
+              the ins and outs of golf. From running the cart barn all the way
+              to maintaining the golf course, even handling tournament
+              operations. He also sat in on lessons as often as he could from
+              the Head Professional Herman Keiser. By the time he graduated, not
+              only was he proficient in all aspects of running a golf course but
+              he also learned the proper mechanics and fundamentals of a golf
+              swing and most importantly how to teach it on to others.
+            </p>
+            <p className='pb-4 indent-8'>
+              After high school Brian joined the U.S. Army as a Combat Engineer
+              and served for 5 years, 1 of those years was spent in Iraq
+              conducting Route Clearance and locating IED’s. During his time in
+              the military, golf stayed a very active part of his life, where he
+              practiced and played regularly. After the Army, Brian became a
+              private contractor for the Military as a UXO Technician, where he
+              cleared old bomb testing sites of explosives and hazardous
+              materials.
+            </p>
+            <p className='pb-4 indent-8'>
+              A few years later Brian attended The Golf Academy of America and
+              received an Associates Degree in Golf Management. That helped him
+              land a job as an Outside Operations Manager for Legendary Golf
+              Instructor David Leadbetter. Here Brian worked side by side with
+              top players on both PGA and LPGA tours, sat in on lessons and
+              assisted with drills. Brian quickly started doing all of the club
+              repair work and learned to fit players and always did loft and lie
+              adjustments for everyone that came to the Leadbetter Golf Academy.
+            </p>
+            <p className='pb-4 indent-8'>
+              Brian started caddying for a few LPGA tour players as a fill in
+              and on his off time worked did club fittings and lessons. Not much
+              later he caught the eye of one of the largest golf club
+              manufacturers in the world, Cobra Golf. Cobra/Puma Golf offered
+              Brian a ambassador position to be a rep for their brand. Along
+              with Cobra/Puma, Brian also landed sponsorship deals with
+              Aero-Swing, iconic training to increase club head speed and power.
+              Blue Tees Rangefinders which are top of the line rangefinders and
+              Sweet Rollz Putter grips which are unique and stylish putter
+              grips.
+            </p>
+            <p className='pb-4 indent-8'>
+              Brian is now branding his own name, Golf With Brian. Where he
+              offers exceptional service in club and ball fittings and lessons
+              of all skill levels. Brian’s plan for Golf With Brian is to have
+              everyone enjoy the game and find true happiness on the golf
+              course. He still caddies and travels with a few LPGA tour players
+              and and continues to grow his name.
+            </p>
+            <p className='pb-4 indent-8'>
+              Brian’s unique approach to the game of golf is that he does
+              everything with his clients. He doesn’t instruct, he provides
+              details and examples and displays everything he teaches himself so
+              you get to see it first hand. So you know that everything he has
+              you do, he can do himself.{' '}
+              <em>
+                “That’s why I named the company <strong>Golf With Brian</strong>
+                , to let my clients know that they aren’t alone and that we are
+                doing this together”
+              </em>{' '}
+              .
+            </p>
+            <p className='pb-4 indent-8'>
+              Brian doesn’t market a certain swing type and doesn’t have a
+              “magic cure” for every golfer. He starts from the foundation and
+              instills proper set up fundamentals and invites nothing but the
+              best of habits to come to his clients.{' '}
+              <em>
+                “No matter what, for anything to last, you need a solid
+                foundation.”
+              </em>
+            </p>
+            <p className='pb-4 indent-8'>
+              Brian provides the best all around service for everyone of his
+              clients anything to do with golf he knows it.
+            </p>
+            <p className='pb-4 indent-8'>
+              <em>
+                “The best feeling ever is watching someone succeed and to
+                witness first hand their excitement and knowing you had a part
+                in that!!! No better feeling”
+              </em>{' '}
+              .
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
