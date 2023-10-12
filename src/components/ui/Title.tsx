0361fedbb4aa36ea2d655112968db012de43ccbc
James Barber <jpbarber34@gmail.com>
2023-10-12 14:00:44 -0400

    Rework title section to have background image with text.

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -4,13 +4,14 @@ import { faGolfBall } from '@fortawesome/free-solid-svg-icons';
 
 export default function Title() {
   return (
-    <div className='bg-brianBlue text-white grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
-      <div className='pt-16 m-auto text-3xl xl:text-5xl w-2/3 font-extrabold xl:col-span-2'>
+    <div className='py-16 relative text-white title-image'>
+      <div className='absolute inset-0 bg-brianBlue opacity-80'></div>
+      <div className='order-2 lg:order-1 pt-16 m-auto mt-12 md:mt-24 lg:mt-28 text-3xl xl:text-5xl w-2/3 font-extrabold xl:col-span-2 text-center relative'>
         <h2>
           There's no magic cure, let's get better together, one swing at a time!
         </h2>
-        <div className='grid grid-cols-1 xl:grid-cols-2 justify-between pt-12'>
-          <div className='xl:text-2xl w-64'>
+        <div className='grid grid-cols-1 xl:grid-cols-2 pt-12 mx-auto'>
+          <div className='xl:text-2xl w-64 mx-auto'>
             <Link href='https://www.cobragolf.com/'>
               <button className='bg-slate-900 border-solid border-2 border-slate-900 hover:bg-white hover:text-black text-white font-bold py-2 px-4 rounded-md'>
                 <a>
@@ -20,7 +21,7 @@ export default function Title() {
               </button>
             </Link>
           </div>
-          <div className='xl:text-2xl w-64 pt-4 xl:pt-0'>
+          <div className='xl:text-2xl w-64 pt-4 xl:pt-0 mx-auto'>
             <Link href='https://www.aero-swing.com/'>
               <button className='bg-clear border-solid border-2 hover:border-black hover:text-black text-white font-bold py-2 px-4 rounded-md'>
                 <a>
@@ -33,12 +34,12 @@ export default function Title() {
         </div>
       </div>
       <div>
-        <img
+        {/* <img
           className='m-auto mt-24 mb-8 border-solid border-4 border-neutral-50 rounded'
           src='/brian_cobra.jpeg'
           width={400}
           alt='Golf with Brian Swing Coach Brian Conine'
-        />
+        /> */}
       </div>
     </div>
   );
