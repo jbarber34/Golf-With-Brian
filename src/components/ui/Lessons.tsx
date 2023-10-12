1c63052b1e452ccbb981ad3b9f934c822c34d36b
James Barber <jpbarber34@gmail.com>
2023-10-12 14:00:43 -0400

    Add modal for lession perk information

--- src/components/ui/Lessons.tsx
+++ src/components/ui/Lessons.tsx
@@ -1,4 +1,95 @@
-export default function Lessons() {
+import React from 'react';
+import LessonPlanModal from './LessonPlanModal';
+
+interface Props {
+  modalThreeIsOpen: any;
+  openModalThree: any;
+  closeModalThree: any;
+  modalSixIsOpen: any;
+  openModalSix: any;
+  closeModalSix: any;
+  modalTenIsOpen: any;
+  openModalTen: any;
+  closeModalTen: any;
+}
+
+export default function Lessons({
+  modalThreeIsOpen,
+  openModalThree,
+  closeModalThree,
+  modalSixIsOpen,
+  openModalSix,
+  closeModalSix,
+  modalTenIsOpen,
+  openModalTen,
+  closeModalTen,
+}: Props) {
+  const lessonPackages = {
+    three: [
+      {
+        title: '3-Lesson Package',
+        feature:
+          'Includes Trackman Launch monitor for accurate readings and better understanding of how we are going to improve your game.',
+      },
+      {
+        feature:
+          'An initial spec out to show your before and after (video and numbers).',
+      },
+      {
+        feature: 'One free round of golf during use of package.',
+      },
+      {
+        feature:
+          "If you eat at the restaurant one time during your package and text me a pic of the receipt. I'll reimburse you $32.",
+      },
+    ],
+    six: [
+      {
+        title: '6-Lesson Package',
+        feature:
+          'Includes Trackman Launch monitor for accurate readings and better understanding of how we are going to improve your game.',
+      },
+      {
+        feature:
+          'An initial spec out to show your before and after (video and numbers).',
+      },
+      {
+        feature: 'Two free rounds of golf during use of package.',
+      },
+      {
+        feature: 'One complimentary ball fitting (Titleist golf balls).',
+      },
+      {
+        feature:
+          "If you eat at the restaurant three times during your package and text me a pic of the receipt. I'll reimburse you $65.",
+      },
+    ],
+    ten: [
+      {
+        title: '10-Lesson Package',
+        feature:
+          'Includes Trackman Launch monitor for accurate readings and better understanding of how we are going to improve your game.',
+      },
+      {
+        feature:
+          'An initial spec out to show your before and after (video and numbers).',
+      },
+      {
+        feature: 'Three free rounds of golf during use of package.',
+      },
+      {
+        feature: 'One complimentary ball fitting (Titleist golf balls).',
+      },
+      {
+        feature:
+          'One complimentary on-course training. Approaching greens, course management, fairway bunkers, and lots of short game.',
+      },
+      {
+        feature:
+          "If you eat at the restaurant five times during your package and text me a pic of the receipt. I'll reimburse you $100.",
+      },
+    ],
+  };
   return (
     <section id='lessons' className='my-20 text-center'>
       <h2
@@ -256,28 +347,28 @@ export default function Lessons() {
 
             <div className='text-gray-500 dark:text-gray-400'></div>
             <div>
-              <a
-                href='#'
-                className='text-white block w-full bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:focus:ring-blue-900'
-              >
-                Learn more
-              </a>
+              <LessonPlanModal
+                content={lessonPackages.three}
+                open={openModalThree}
+                close={closeModalThree}
+                modalIsOpened={modalThreeIsOpen}
+              ></LessonPlanModal>
             </div>
             <div>
-              <a
-                href='#'
-                className='text-white block w-full bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:focus:ring-blue-900'
-              >
-                Learn more
-              </a>
+              <LessonPlanModal
+                content={lessonPackages.six}
+                open={openModalSix}
+                close={closeModalSix}
+                modalIsOpened={modalSixIsOpen}
+              ></LessonPlanModal>
             </div>
             <div>
-              <a
-                href='#'
-                className='text-white block w-full bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:focus:ring-blue-900'
-              >
-                Learn more
-              </a>
+              <LessonPlanModal
+                content={lessonPackages.ten}
+                open={openModalTen}
+                close={closeModalTen}
+                modalIsOpened={modalTenIsOpen}
+              ></LessonPlanModal>
             </div>
           </div>
           <div className='text-xs text-gray-500 text-left pl-4'>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,34 @@ import Title from '~/components/ui/Title';
 import Lessons from '~/components/ui/Lessons';
 import Sponsors from '~/components/ui/Sponsors';
 import ImageCarousel from '~/components/ui/ImageCarousel';
+import { useDisclosure } from '@mantine/hooks';
+import mod from 'zod/lib';
 
 function Home() {
+  const [modalThreeIsOpen, { open: modalThreeOpen, close: modalThreeClosed }] =
+    useDisclosure(false);
+  const [modalSixIsOpen, { open: modalSixOpen, close: modalSixClosed }] =
+    useDisclosure(false);
+  const [modalTenIsOpen, { open: modalTenOpen, close: modalTenClosed }] =
+    useDisclosure(false);
+
   return (
     <div>
       <Title />
       <Features />
       {/* <Services /> */}
       <Testimonials />
-      <Lessons />
+      <Lessons
+        modalThreeIsOpen={modalThreeIsOpen}
+        modalSixIsOpen={modalSixIsOpen}
+        modalTenIsOpen={modalTenIsOpen}
+        openModalThree={modalThreeOpen}
+        closeModalThree={modalThreeClosed}
+        openModalSix={modalSixOpen}
+        closeModalSix={modalSixClosed}
+        openModalTen={modalTenOpen}
+        closeModalTen={modalTenClosed}
+      />
       <ImageCarousel />
       <Sponsors />
     </div>
